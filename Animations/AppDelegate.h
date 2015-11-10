7363f59cea91f0ac667a358927b3af394096a830
Juan Pablo Marzetti <yonpols@gmail.com>
2015-11-10 14:41:12 -0800

    Initial Commit

---
+++ Animations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animations
+//
+//  Created by Juan Pablo Marzetti on 11/10/15.
+//  Copyright © 2015 Juan Pablo Marzetti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
