94628948f831d914f0ed313a0143d35a0e636d51
Juan Pablo Marzetti <jmarzetti@groupon.com>
2015-11-24 12:23:49 -0800

    Animations

--- Animations/ViewController.m
+++ Animations/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *image;
 
 @end
 
@@ -17,6 +18,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)tapped:(id)sender {
+    CGPoint imageCenter = self.image.center;
+    
+    [UIView animateWithDuration:1 delay:0.2 usingSpringWithDamping:1 initialSpringVelocity:0 options:0
+                     animations:^{
+                         self.image.center = CGPointMake(imageCenter.x, imageCenter.y + 300);
+                         self.image.transform = CGAffineTransformMakeScale(3, 3);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.2 animations:^{
+            self.image.transform = CGAffineTransformRotate(self.image.transform, 10 * M_PI / 100);
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:0.3 delay:0.1
+                                options:UIViewAnimationOptionAutoreverse+UIViewAnimationOptionRepeat+UIViewAnimationOptionCurveLinear
+                             animations:^{
+                                 self.image.transform = CGAffineTransformRotate(self.image.transform, -20 * M_PI / 100);
+                             } completion:^(BOOL finished) {
+                             }];
+        }];
+    }];
+    
+//    [UIView animateWithDuration:0.3 animations:^{
+//        self.image.center = CGPointMake(imageCenter.x, imageCenter.y + 300);
+//        self.image.transform = CGAffineTransformMakeScale(3, 3);
+//    } completion:^(BOOL finished) {
+//        [UIView animateWithDuration:0.2 animations:^{
+//            self.image.transform = CGAffineTransformRotate(self.image.transform, 10 * M_PI / 100);
+//        } completion:^(BOOL finished) {
+//            [UIView animateWithDuration:0.3 delay:0.1
+//                                options:UIViewAnimationOptionAutoreverse+UIViewAnimationOptionRepeat+UIViewAnimationOptionCurveLinear
+//                             animations:^{
+//                self.image.transform = CGAffineTransformRotate(self.image.transform, -20 * M_PI / 100);
+//            } completion:^(BOOL finished) {
+//            }];
+//        }];
+//    }];
 }
 
 - (void)didReceiveMemoryWarning {
